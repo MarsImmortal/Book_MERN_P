b6bf4d3e2485181c4a1e8891d12431d3fc559215
MarsImmortal <s222198007@deakin.edu.au>
2023-09-18 16:21:42 +1000

    add functionality to connect to mongo db

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,25 @@
 import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
 
 const app = express();
 
+app.get('/', (req, res) => {
+    console.log(req);
+    return res.status(243).send('Welcome to the mern stack project');
+});
+
+
+mongoose.connect(mongoDBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('connected to mongodb');
+    app.listen(PORT, () => {
+        console.log('listening on port ' + PORT);
+    });
+})
+.catch((err) => {
+    console.log('error connecting to mongodb', err);
+});
