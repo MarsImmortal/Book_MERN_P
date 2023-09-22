a6614f9735fc84cf98b2ee482438ac3ba0acbafb
MarsImmortal <s222198007@deakin.edu.au>
2023-09-22 14:39:54 +1000

    add code to use tailwind features for designing

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import { PORT, mongoDBURL } from './config.js';
 import mongoose from 'mongoose';
-
+import { Book } from './models/bookModel.js';
+import booksRoute from './routes/booksRoutes.js';
+import cors from 'cors';
+ 
+ 
 const app = express();
+
+app.use(express.json());
+
+app.use(cors()); 
 
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(243).send('Welcome to the mern stack project');
 });
 
+app.use('/books', booksRoute)
 
 mongoose.connect(mongoDBURL, {
     useNewUrlParser: true,
